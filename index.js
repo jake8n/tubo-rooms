32634471d86b6d087ad03e252468f396bd66767a
Jake Patterson <jake@patt.me>
2020-10-28 00:01:29 +0000

    wip: edit multiple files

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const io = require("socket.io")({});
 const docs = new Map();
 io.on("connection", (socket) => {
   const { room } = socket.handshake.query;
-  socket.join(room).emit("joined", docs.get(room));
-  socket.on("update", (changes) => {
-    socket.to(room).emit("update", changes);
-  });
-  socket.on("sync", (doc) => {
-    docs.set(room, doc);
+  if (!docs.get(room)) {
+    docs.set(room, { js: "", html: "", css: "" });
+  }
+  socket.join(room);
+  ["js", "html", "css"].forEach((value) => {
+    socket.emit(`init:${value}`, docs.get(room)[value]);
+    socket.on(`update:${value}`, (changes) => {
+      console.log("update", value);
+      socket.to(room).emit(`update:${value}`, changes);
+    });
+    socket.on(`sync:${value}`, (doc) => {
+      const docPrevious = docs.get(room);
+      docPrevious[value] = doc;
+      docs.set(room, docPrevious);
+    });
   });
 });
 io.listen(process.env.PORT);
