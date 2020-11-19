907628d9ba00a1a6c0a6eaecf6c9dc2cdfad6edd
Jake Patterson <jake@patt.me>
2020-11-19 22:23:16 +0000

    wip: refactor socket events

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const io = require("socket.io")({});
-// TODO: make persistent
-const docs = new Map();
+
 io.on("connection", (socket) => {
-  const { room } = socket.handshake.query;
-  if (!docs.get(room)) {
-    docs.set(room, { js: "", html: "", css: "" });
-  }
-  socket.join(room);
-  ["js", "html", "css"].forEach((value) => {
-    socket.emit(`init:${value}`, docs.get(room)[value]);
-    socket.on(`update:${value}`, (changes) => {
-      socket.to(room).emit(`update:${value}`, changes);
-    });
-    socket.on(`sync:${value}`, (doc) => {
-      const docPrevious = docs.get(room);
-      docPrevious[value] = doc;
-      docs.set(room, docPrevious);
-    });
+  socket.on("join-room", (room) => {
+    socket.join(room);
+
+    if (io.sockets.adapter.rooms[room].length <= 1) {
+      socket.emit("room-created");
+    } else {
+      socket.to(room).emit("request-for-state");
+    }
+
+    socket.on("response-for-state", (value) =>
+      socket.to(room).emit("room-joined", value)
+    );
+
+    socket.on("transaction", (transaction) =>
+      socket.to(room).emit("transaction", transaction)
+    );
   });
 });
+
 io.listen(process.env.PORT || 80);
